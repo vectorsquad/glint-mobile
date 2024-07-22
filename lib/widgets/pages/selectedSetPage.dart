7d0f4072dd55755b2d84ba60cf259a0c8730a485
Alan Phan <125511623+Alphiel@users.noreply.github.com>
2024-07-22 15:55:55 -0400

    minor UI updates

--- lib/widgets/pages/selectedSetPage.dart
+++ lib/widgets/pages/selectedSetPage.dart
@@ -11,114 +11,114 @@ class SelectedSetPage extends StatelessWidget {
     return PageBuilderRound(
         child: SingleChildScrollView(
             child: Form(
-              key: formSignupKey,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text(
-                    'SET NAME',
-                    style: TextStyle(
-                      fontSize: 30.0,
-                      fontWeight: FontWeight.w900,
-                      color: lightColorScheme.primary,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 30.0,
-                  ),
-                  SizedBox(
-                    width: double.infinity,
-                    height: 420.0,
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: lightColorScheme.tertiary,
-                          textStyle: const TextStyle(fontSize: 24)),
-                      child: const Text('Card 1'),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 20.0,
-                  ),
-                  Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-                    SizedBox(
-                      width: 100.0,
-                      height: 70.0,
-                      child: ElevatedButton(
-                          onPressed: () {
+      key: formSignupKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            props.name,
+            style: TextStyle(
+              fontSize: 30.0,
+              fontWeight: FontWeight.w900,
+              color: lightColorScheme.primary,
+            ),
+          ),
+          const SizedBox(
+            height: 30.0,
+          ),
+          SizedBox(
+            width: double.infinity,
+            height: 420.0,
+            child: ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: lightColorScheme.tertiary,
+                  textStyle: const TextStyle(fontSize: 24)),
+              child: const Text('Card 1'),
+            ),
+          ),
+          const SizedBox(
+            height: 20.0,
+          ),
+          Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+            SizedBox(
+              width: 100.0,
+              height: 70.0,
+              child: ElevatedButton(
+                  onPressed: () {
 // PREV CARD
-                          },
-                          style: ElevatedButton.styleFrom(
-                              backgroundColor: lightColorScheme.primary,
-                              textStyle: const TextStyle(fontSize: 14)),
-                          child: const Icon(
-                            Icons.chevron_left,
-                            size: 35.0,
-                            color: Colors.white,
-                          )),
-                    ),
-                    RawMaterialButton(
-                        onPressed: () =>
-                            pushRoute(
-                                context,
-                                DeckEditor(
-                                    props: props
-                                )
-                            ),
-                        elevation: 2.0,
-                        fillColor: lightColorScheme.primary,
-                        padding: const EdgeInsets.all(15.0),
-                        shape: const CircleBorder(),
-                        child: const Icon(
-                          Icons.edit,
-                          size: 40.0,
-                          color: Colors.white,
-                        )),
-                    SizedBox(
-                      width: 100.0,
-                      height: 70.0,
-                      child: ElevatedButton(
-                          onPressed: () {},
-                          style: ElevatedButton.styleFrom(
-                              backgroundColor: lightColorScheme.primary,
-                              textStyle: const TextStyle(fontSize: 14)),
-                          child: const Icon(
-                            Icons.chevron_right,
-                            size: 35.0,
-                            color: Colors.white,
-                          )),
-                    ),
-                  ]),
-                  const SizedBox(
-                    height: 30.0,
+                  },
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: lightColorScheme.primary,
+                      textStyle: const TextStyle(fontSize: 14)),
+                  child: const Icon(
+                    Icons.chevron_left,
+                    size: 35.0,
+                    color: Colors.white,
+                  )),
+            ),
+            RawMaterialButton(
+                onPressed: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => RouteBuilder(
+                          child: SingleChildScrollView(
+                              child: EditDeckPage(props: props))),
+                    )),
+                elevation: 2.0,
+                fillColor: lightColorScheme.primary,
+                padding: const EdgeInsets.all(15.0),
+                shape: const CircleBorder(),
+                child: const Icon(
+                  Icons.edit,
+                  size: 40.0,
+                  color: Colors.white,
+                )),
+            SizedBox(
+              width: 100.0,
+              height: 70.0,
+              child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: lightColorScheme.primary,
+                      textStyle: const TextStyle(fontSize: 14)),
+                  child: const Icon(
+                    Icons.chevron_right,
+                    size: 35.0,
+                    color: Colors.white,
+                  )),
+            ),
+          ]),
+          const SizedBox(
+            height: 30.0,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (e) => const Text("not implemented"),
+                      ));
+                },
+                child: Text(
+                  'Start studying',
+                  style: TextStyle(
+                    fontSize: 22.0,
+                    fontWeight: FontWeight.bold,
+                    color: lightColorScheme.primary,
                   ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      GestureDetector(
-                        onTap: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (e) => const Text("not implemented"),
-                              ));
-                        },
-                        child: Text(
-                          'Start studying',
-                          style: TextStyle(
-                            fontSize: 22.0,
-                            fontWeight: FontWeight.bold,
-                            color: lightColorScheme.primary,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 40.0,
-                  ),
-                ],
+                ),
               ),
-            )));
+            ],
+          ),
+          const SizedBox(
+            height: 40.0,
+          ),
+        ],
+      ),
+    )));
   }
 }
